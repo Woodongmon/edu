ee19df82917121135e65acd8e8a1e0c27ab38e2e
Woodong <woodongwoodong123@naver.com>
2025-10-27 11:55:13 +0900

    dht11 uploaded

---
+++ arm/11_DHT11/Core/Inc/dht11.h
@@ -0,0 +1,40 @@
+/*
+ * dht11.h
+ *
+ *  Created on: Oct 27, 2025
+ *      Author: user21
+ */
+
+#ifndef INC_DHT11_H_
+#define INC_DHT11_H_
+
+#include "stm32f4xx_hal.h"
+#include "stdint.h"
+#include "delay_us.h"
+#include "tim.h"
+#include "stdio.h"
+#include "stdbool.h"
+
+enum
+{
+	INPUT,
+	OUTPUT
+};
+
+
+typedef struct
+{
+	GPIO_TypeDef 	*port;
+	uint16_t		pin;
+	uint8_t			temperature;
+	uint8_t			humidity;
+}DHT11;
+
+void dht11Init(DHT11 *dht, GPIO_TypeDef *port, uint16_t pin);
+void dht11GpioMode(DHT11 *dht, uint8_t mode);
+uint8_t dht11Read(DHT11 *dht);
+
+
+
+
+#endif /* INC_DHT11_H_ */
